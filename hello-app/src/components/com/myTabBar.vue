6c2a01f4687ea8da6f6a39a448762c22cb66d515
public16to <public@16to.com>
2018-09-08 17:46:54 +0800

    font

--- hello-app/src/components/com/myTabBar.vue
+++ hello-app/src/components/com/myTabBar.vue
@@ -1,76 +1,70 @@
 <template>
   <wxc-tab-bar :tab-titles="tabTitles"
                :tab-styles="tabStyles"
-               title-type="icon"
+               title-type="iconFont"
                @wxcTabBarCurrentTabSelected="wxcTabBarCurrentTabSelected">
-    <div class="item-container" :style="contentStyle"><text>首页123</text></div>
-    <div class="item-container" :style="contentStyle"><text>特别推荐666</text></div>
-    <div class="item-container" :style="contentStyle"><text>消息中心888</text></div>
+    <div class="item-container" :style="contentStyle"><text>首页</text></div>
+    <div class="item-container" :style="contentStyle"><text>特别推荐</text></div>
+    <div class="item-container" :style="contentStyle"><text>消息中心</text></div>
     <div class="item-container" :style="contentStyle"><text>我的主页</text></div>
   </wxc-tab-bar>
 </template>
 
-<style lang="less">
+<style scoped>
   .item-container {
     width: 750px;
     background-color: #f2f3f4;
     align-items: center;
     justify-content: center;
   }
-  .desc-tag{
-    border-bottom-left-radius: 14px !important;
-    height: 32px !important;
-    width: 32px !important;
-  }
 </style>
 <script>
   import { WxcTabBar, Utils } from 'weex-ui';
   var Config={
-    // 正常模式的tab title配置
-    tabTitles: [
+    // 使用 iconFont 模式的tab title配置
+    tabIconFontTitles: [
       {
         title: '首页',
-        icon: 'https://gw.alicdn.com/tfs/TB1MWXdSpXXXXcmXXXXXXXXXXXX-72-72.png',
-        activeIcon: 'https://gw.alicdn.com/tfs/TB1kCk2SXXXXXXFXFXXXXXXXXXX-72-72.png'
+        codePoint: '\ue623'
       },
       {
         title: '特别推荐',
-        icon: 'https://gw.alicdn.com/tfs/TB1ARoKSXXXXXc9XVXXXXXXXXXX-72-72.png',
-        activeIcon: 'https://gw.alicdn.com/tfs/TB19Z72SXXXXXamXFXXXXXXXXXX-72-72.png'
+        codePoint: '\ue608'
       },
       {
         title: '消息中心',
-        icon: 'https://gw.alicdn.com/tfs/TB1VKMISXXXXXbyaXXXXXXXXXXX-72-72.png',
-        activeIcon: 'https://gw.alicdn.com/tfs/TB1aTgZSXXXXXazXFXXXXXXXXXX-72-72.png',
+        codePoint: '\ue752',
         badge: 5
       },
       {
         title: '我的主页',
-        icon: 'https://gw.alicdn.com/tfs/TB1Do3tSXXXXXXMaFXXXXXXXXXX-72-72.png',
-        activeIcon: 'https://gw.alicdn.com/tfs/TB1LiNhSpXXXXaWXXXXXXXXXXXX-72-72.png',
+        codePoint: '\ue601',
         dot: true
       }
     ],
-    tabStyles: {
+    tabIconFontStyles: {
       bgColor: '#FFFFFF',
       titleColor: '#666666',
       activeTitleColor: '#3D3D3D',
       activeBgColor: '#FFFFFF',
       isActiveTitleBold: true,
-      iconWidth: 70,
-      iconHeight: 70,
       width: 160,
       height: 120,
       fontSize: 24,
       textPaddingLeft: 10,
-      textPaddingRight: 10
+      textPaddingRight: 10,
+      iconFontSize: 50,
+      iconFontMarginBottom: 8,
+      iconFontColor: '#333333',
+      activeIconFontColor: 'red',
+      iconFontUrl: '//at.alicdn.com/t/font_501019_mauqv15evc1pp66r.ttf'
     }
   };
   export default {
     components: { WxcTabBar },
     data: () => ({
-      tabTitles: Config.tabTitles,
-      tabStyles: Config.tabStyles
+      tabTitles: Config.tabIconFontTitles,
+      tabStyles: Config.tabIconFontStyles
     }),
     created () {
       const tabPageHeight = Utils.env.getPageHeight();
